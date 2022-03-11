bf5537a2ad4c45c5ff6ff7c0e55ed19c60de0284
Andressa Almeida <andressa.vdk93@gmail.com>
2022-03-11 13:30:02 -0300

    inclusão controllers, repositories, routes e services

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
 import "reflect-metadata"; 
 import express, { response } from 'express';
+import {router} from "./routes";
 
 import "./database/index";
 
 const app = express();
 
+app.use(express.json())
+
+app.use(router);
 
 app.listen(3000, () => console.log('Server is running'))
 
