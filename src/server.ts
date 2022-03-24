338ec9e6cbba45d27380ae04de5116442aa18fb9
Andressa Almeida <andressa.vdk93@gmail.com>
2022-03-24 12:53:34 -0300

    adicionada coluna de senhas com criptografia

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,21 @@
 import "reflect-metadata"; 
 import express, { Request, Response, NextFunction } from 'express';
-import {router} from "./routes";
-import "./database/index";
 import "express-async-errors";
+import {router} from "./routes";
+
+
+import "./database/index";
+
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(router);
 
 app.use(
     (err: Error, request: Request, response: Response, next: NextFunction) => {
-        if(err instanceof Error) {
+        if (err instanceof Error) {
             return response.status(400).json({
                 error: err.message,
             });
@@ -23,7 +26,7 @@ app.use(
             message: "Internal Server Error",
         });
     }
-)
+);
 
 app.listen(3000, () => console.log('Server is running'))
 
